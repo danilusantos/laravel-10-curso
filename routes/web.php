d680b8c1209209a644b1d8ac3ce245e4c7c88364
Danilo Santos <danilondosantos@gmail.com>
2024-04-26 14:11:51 -0300

    feat: :sparkles: configurando a rota de supports

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,14 @@
 <?php
 
+use App\Http\Controllers\Admin\SupportController;
 use App\Http\Controllers\Site\ContactController;
 use Illuminate\Support\Facades\Route;
+
+Route::name('supports.')
+    ->controller(SupportController::class)
+    ->prefix('supports')
+    ->group(function () {
+        Route::get('/', 'index')->name('index');
+    });
 
 Route::get('/contato', [ContactController::class, 'index']);
