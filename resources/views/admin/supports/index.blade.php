7d3bebf0446af34b0541c9c98b15a3d2e32377b3
Danilo Santos <danilondosantos@gmail.com>
2024-04-30 15:05:08 -0300

    feat: :sparkles: impressão dos dados na blade

--- resources/views/admin/supports/index.blade.php
+++ resources/views/admin/supports/index.blade.php
@@ -1,1 +1,22 @@
 <h1>Listagem Suporte</h1>
+
+<table>
+    <caption></caption>
+    <thead>
+        <tr>
+            <th>Assunto</th>
+            <th>Status</th>
+            <th>Descrição</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($supports as $support)
+            <tr>
+                <td>{{ $support->subject }}</td>
+                <td>{{ $support->status }}</td>
+                <td>{{ $support->body }}</td>
+                <td> - </td>
+            </tr>
+        @endforeach
+    </tbody>
+</table>
