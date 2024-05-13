ca4c66c3318033e9e5dc0fe668289972df9e7d31
Danilo Santos <danilondosantos@gmail.com>
2024-05-13 14:38:49 -0300

    feat: :sparkles: implementar repositories no laravel 10

--- app/Repositories/SupportEloquentORM.php
+++ app/Repositories/SupportEloquentORM.php
@@ -5,23 +5,43 @@ use App\DTO\{
     CreateSupportDTO,
     UpdateSupportDTO
 };
+use App\Models\Support;
 use stdClass;
 
 class SupportEloquentORM implements SupportRepositoryInterface
 {
+    public function __construct(
+        protected Support $model
+    ) {
+    }
+
     public function getAll(string $filter = null): array
     {
-
+        return $this->model
+            ->where(function ($query) use ($filter) {
+                if ($filter) {
+                    $query->where('subject', $filter);
+                    $query->orWhere('body', 'like', "%{$filter}%");
+                }
+            })
+            ->paginate()
+            ->toArray();
     }
 
     public function findOne(string $id): stdClass|null
     {
+        if (! $support = $this->model->find($id)) {
+            return null;
+        }
 
+        return (object) $support->toArray();
     }
 
     public function delete(string $id): void
     {
-
+        $this->model
+            ->findOrFail($id)
+            ->delete();
     }
 
     public function new(CreateSupportDTO $dto): stdClass
