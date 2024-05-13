dc91db932afefda82ae647d9fc7e23746985b73d
Danilo Santos <danilondosantos@gmail.com>
2024-05-13 14:57:07 -0300

    feat: finalizando o repositório no laravel 10

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,8 +3,8 @@
 namespace App\Providers;
 
 use App\Repositories\SupportRepositoryInterface;
+use App\Repositories\SupportEloquentORM;
 use Illuminate\Support\ServiceProvider;
-use SupportEloquentORM;
 
 class AppServiceProvider extends ServiceProvider
 {
--- resources/views/admin/supports/index.blade.php
+++ resources/views/admin/supports/index.blade.php
@@ -16,19 +16,20 @@
         @foreach ($supports as $support)
             <tr>
                 <td>
-                    <a href="{{ route('supports.show', ['id' => $support->id]) }}">
-                        {{ $support->subject }}
+                    <a href="{{ route('supports.show', ['id' => $support['id']]) }}">
+                        {{ $support['subject'] }}
                     </a>
                 </td>
-                <td>{{ $support->status }}</td>
-                <td>{{ $support->body }}</td>
+                <td>{{ $support['status'] }}</td>
+                <td>{{ $support['body'] }}</td>
                 <td>
-                    <a href="{{ route('supports.edit', ['id' => $support->id]) }}">
+                    <a href="{{ route('supports.edit', ['id' => $support['id']]) }}">
                         Editar
                     </a>
                 </td>
                 <td>
-                    <form action="{{ route('supports.destroy', ['id' => $support->id]) }}" method="POST" id="formDeletar">
+                    <form action="{{ route('supports.destroy', ['id' => $support['id']]) }}" method="POST"
+                        id="formDeletar">
                         @csrf
                         @method('DELETE')
                         <button type="submit">Remover</button>
